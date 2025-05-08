a14ed7820d8ed15629e5c0d0630d8bb5721ff15a
Ibrar Ahmad <hiibrarahmad@gmail.com>
2025-05-08 16:33:07 +0500

    cursor

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,23 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+// Get the button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Show/hide the button on scroll
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Smooth scroll to top when clicked
+scrollToTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
